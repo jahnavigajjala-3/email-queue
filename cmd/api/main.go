9d3f585f0276b3430eacfff939ca46a028be4a6b
jahnavigajjala3-commits <jahnavigajjala3@gmail.com>
2026-06-25 10:35:07 +0530

    add api server with send-emails endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
+
 import (
-	"encoding/json"
+    "encoding/json"
     "fmt"
     "net/http"
     "context"
     "github.com/jahnavigajjala-3/email-queue/internal/job"
     "github.com/redis/go-redis/v9"
 )
+
+var ctx = context.Background()
+var rdb *redis.Client
+
 func main() {
     rdb = redis.NewClient(&redis.Options{
         Addr: "localhost:6379",
@@ -14,4 +19,26 @@ func main() {
     http.HandleFunc("/send-emails", handleSendEmails)
     fmt.Println("API server running on port 8080")
     http.ListenAndServe(":8080", nil)
+}
+
+func handleSendEmails(w http.ResponseWriter, r *http.Request) {
+    var request struct {
+        Recipients []string `json:"recipients"`
+        Subject    string   `json:"subject"`
+        Body       string   `json:"body"`
+    }
+    json.NewDecoder(r.Body).Decode(&request)
+
+    for _, recipient := range request.Recipients {
+        j := job.Job{
+            To:      recipient,
+            Subject: request.Subject,
+            Body:    request.Body,
+        }
+        data, _ := json.Marshal(j)
+        rdb.LPush(ctx, "email_queue", data)
+    }
+
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(map[string]string{"status": "jobs queued"})
 }
--- go.mod
+++ go.mod
@@ -2,3 +2,8 @@ module github.com/jahnavigajjala/email-queue
 
 go 1.24
 
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/redis/go-redis/v9 v9.21.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
